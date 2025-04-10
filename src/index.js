1b23d6e825eb06748a963126b54538b911e30648
cvan90 <cvanv90@yahoo.com>
2025-04-10 11:44:42 -0400

    updated code to link sqlight 3 to database

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-let _dbname = "src\data\Dashboard Project.db"
+const sqlite3 = require('sqlite3').verbose();
 
-let db = new sqlite3.Database(_dbname, (err) => {
+let _dbname = "src\\data\\Dashboard Project.db"
+
+let db = new sqlite3.Database(_dbname, sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Connected successfully.');
   });
-  
